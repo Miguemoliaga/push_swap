9a6bc93951207f3289865559dd15b931170af850
Miguel Martinez oliaga <mmartine@c1r16s2.42madrid.com>
2023-04-03 20:15:30 +0200

    push movement finished, rot .c created (empty)

---
+++ mov_push.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mov_push.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmartine <mmartine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 19:39:34 by mmartine          #+#    #+#             */
+/*   Updated: 2023/04/03 20:13:56 by mmartine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push(t_list **src, t_list **dst)
+{
+	t_list	*src_head;
+	t_list	*dst_head;
+
+	if (!*src)
+		return ;
+	src_head = (*src)-> next;
+	dst_head = *src;
+	dst_head -> next = *dst;
+	*dst = dst_head;
+	*src = src_head;
+}
+
+void	push_a(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_b, stack_a);
+	write(1, "pa\n", 3);
+}
+
+void	push_b(t_list **stack_a, t_list **stack_b)
+{
+	push(stack_a, stack_b);
+	write(1, "pb\n", 3);
+}
